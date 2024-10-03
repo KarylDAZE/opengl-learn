d384217982ba42621e205e485a0645ffbfca3db8
KarylDAZE <980596745@qq.com>
2024-10-03 22:58:24 +0800

    add: MSAA

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
     GLFWwindow *window = glfwCreateWindow(window_width, window_heigt, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -69,6 +70,7 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_CULL_FACE);
+    glEnable(GL_MULTISAMPLE);
 
     float vertices[] = {
         // positions    // normals        // texture coords
